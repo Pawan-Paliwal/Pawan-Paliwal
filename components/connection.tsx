ab835afcd5cb2b91aaaa36c4a84a6ebd17e3b1ed
Tejas <tejassharma2021@outlook.com>
2025-02-15 11:46:28 +0530

    FIX: social links and cts

--- components/connection.tsx
+++ components/connection.tsx
@@ -1,51 +1,60 @@
-'use client';
+"use client";
 
-import Image from 'next/image';
-import Button from './button';
-import Reveal from './reveal';
-import { toast } from 'sonner';
-import { CopyToClipboard } from 'react-copy-to-clipboard';
-import { emailAddress } from '@/constants';
+import Image from "next/image";
+import Button from "./button";
+import Reveal from "./reveal";
+import { toast } from "sonner";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import { emailAddress } from "@/constants";
 
 export default function Connection() {
-	return (
-		<div id='contact' className='relative w-full pt-52 sm:pt-20'>
-			<img
-				src='/images/dexterity-wave.svg'
-				className='absolute left-0 top-0 w-full sm:hidden'
-			/>
-			<div className='flex justify-center'>
-				<div className='relative'>
-					<h1 className="font-main text-7xl leading-[90px] text-secondary-700 text-center lg:text-[65px] lg:leading-tight sm:text-5xl sm:leading-[1.2]">
-						<Reveal direction='down'>
-							<span className='block'>
-								Thanks for stopping by!
-							</span>
-						</Reveal>
-						<Reveal direction='down'>
-							<span className='block'>
-								Stay connected
-							</span>
-						</Reveal>
-					</h1>
+  return (
+    <div id="contact" className="relative w-full pt-52 sm:pt-20">
+      <img
+        src="/images/dexterity-wave.svg"
+        className="absolute left-0 top-0 w-full sm:hidden"
+      />
+      <div className="flex justify-center">
+        <div className="relative">
+          <h1 className="text-center font-main text-7xl leading-[90px] text-secondary-700 lg:text-[65px] lg:leading-tight sm:text-5xl sm:leading-[1.2]">
+            <Reveal direction="down">
+              <span className="block">Thanks for stopping by!</span>
+            </Reveal>
+            <Reveal direction="down">
+              <span className="block">Stay connected</span>
+            </Reveal>
+          </h1>
 
-					<Image
-						src='/images/smiley.svg'
-						alt='thankyou'
-						width={50}
-						height={50}
-						className='absolute right-0 -top-[15%] animate-float'
-					/>
+          <Image
+            src="/images/smiley.svg"
+            alt="thankyou"
+            width={50}
+            height={50}
+            className="absolute -top-[15%] right-0 animate-float"
+          />
 
-					<Reveal direction='down' className='mt-8 text-center'>
-						<CopyToClipboard
-							text={emailAddress}
-							onCopy={() => toast('Email copied')}>
-							<Button>Get in Touch</Button>
-						</CopyToClipboard>
-					</Reveal>
-				</div>
-			</div>
-		</div>
-	);
+          <Reveal direction="down" className="mt-8 text-center">
+            <div className="flex justify-center gap-4 flex-wrap">
+              <CopyToClipboard
+                text={emailAddress}
+                onCopy={() => toast("Email copied")}
+              >
+                <Button>Get in Touch</Button>
+              </CopyToClipboard>
+              <Button variant="ghost">
+                <a
+                  className="resume-button"
+                  href="/tejas_sharma_resume.pdf"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Download résumé
+                </a>
+              </Button>
+            </div>
+          </Reveal>
+        </div>
+      </div>
+    </div>
+  );
 }
--- components/footer.tsx
+++ components/footer.tsx
@@ -1,40 +1,37 @@
-import Image from 'next/image';
-import Logo from './logo';
-import Divider from './divider';
-import { cn } from '@/lib/utils';
+import Image from "next/image";
+import Logo from "./logo";
+import Divider from "./divider";
+import { cn } from "@/lib/utils";
 
 export default function Footer() {
-	return (
-		<footer className='container pt-16 md:px-4 sm:px-2'>
-			<div className='flex items-center justify-between sm:flex-col'>
-				<div className='sm:mb-8'>
-					<Image src='/images/logo.svg' alt='logo' width={176} height={45} />
-				</div>
-				<SocialIcons />
-			</div>
-			<div className='py-10'>
-				<Divider />
-			</div>
-			<p className='pb-8 text-center font-inter text-sm text-secondary-400'>
-				Code and Design – Me
-			</p>
-		</footer>
-	);
+  return (
+    <footer className="container pt-16 md:px-4 sm:px-2">
+      <div className="flex items-center justify-between sm:flex-col">
+        <div className="sm:mb-8">
+          <Image src="/images/logo.svg" alt="logo" width={176} height={45} />
+        </div>
+        <SocialIcons />
+      </div>
+      <div className="py-10">
+        <Divider />
+      </div>
+      <p className="pb-8 text-center font-inter text-sm text-secondary-400">
+        Code and Design – Me
+      </p>
+    </footer>
+  );
 }
 
 type SocialIconsTypes = {
-	className?: string;
+  className?: string;
 };
 
 export function SocialIcons({ className }: SocialIconsTypes) {
-	return (
-		<div className={cn('flex space-x-4', className)}>
-			<Image src='/images/insta.svg' alt='insta' width={20} height={20} />
-			<Image src='/images/twitter.svg' alt='Twitter' width={20} height={20} />
-			<Image src='/images/whatsapp.svg' alt='WhatsApp' width={20} height={20} />
-			<a href='https://github.com/tejassharma-ts' target='_blank'>
-				<Image src='/images/github.svg' alt='GitHub' width={20} height={20} />
-			</a>
-		</div>
-	);
+  return (
+    <div className={cn("flex space-x-4", className)}>
+      <a href="https://github.com/tejassharma-ts" target="_blank" className="hover:translate-x-1 transition-all">
+        <Image src="/images/github.svg" alt="GitHub" width={20} height={20} />
+      </a>
+    </div>
+  );
 }
